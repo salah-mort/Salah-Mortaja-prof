55a312c347b7dc454b464e84203006b559ffcb7f
Salah Mutaja <salah.mort4@gmail.com>
2024-05-15 17:46:11 +0300

    first-comm

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== SHADOW HEADER ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById('header')
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50 ? header.classList.add('shadow-header')
+    : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+contactMessage=document.getElementById('contact-message')
+
+const sendEmail =(e)=>{
+  e.preventDefault()
+
+  // serviceID - templateID - #form - publicKey
+
+}
+
+contactForm.addEventListener('submit',sendEmail)
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
